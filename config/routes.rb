e0b98a1e797b65fff918f35b61e28be0b8eefeac
Joaquin <joaquinsepulveda@hotmail.com>
2021-03-30 19:59:34 -0300

    Third commit: Add method three to pages, created file three.html.erb, added three to routes
    - Added method three to pages controller
    - Added file three.html.erb to the views
    - Added a view for pages/three

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get 'pages/one'
   get 'pages/two'
   root 'pages#one'
+  get 'pages/three'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
